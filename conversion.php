4cf917323ca676e075dcc2609428680efe31f431
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2024-12-09 10:29:14 +0100

    Add files via upload

---
+++ conversion.php
@@ -0,0 +1,9 @@
+<html>
+<form method="post" action="action.php
+">
+    <label for="temp_celsius">Enter temperature Celsius</label>
+    <input type="number">
+    <input type="submit" value="convert">
+</form>
+
+</html>
